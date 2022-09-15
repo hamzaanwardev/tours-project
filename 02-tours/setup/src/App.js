c168f57a5faae8ab9562c092162d3114a5a5cac0
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-09-15 09:13:20 +0500

    boilerplate

---
+++ 02-tours/setup/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
+
+
+function App() {
+  const [loading, setLoadinng] = useState(false);
+  const [tours, setTours] = useState([]);
+  if(loading) {
+    return (
+    <main>
+      <Loading />
+    </main>
+    );
+  } 
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
+}
+
+export default App
